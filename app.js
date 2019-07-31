a45798b08b2e86752d9af976591c35caf488aad7
PulcoOx - Home <oominimathoo@gmail.com>
2019-07-31 20:26:11 +0200

    add app.js

--- app.js
+++ app.js
@@ -1,25 +1,20 @@
 let express = require('express');
 let app = express();
 // let db = require(`${__dirname}/models/index.js`);
-let bodyParser = require('body-parser');
-
-//configuration body parser
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
 
 /////////////////////////////// CONFIG ROUTEUR ////////////////////////////
 
-let users = require('./routes/user.route');
-
-
+const user = require('./routes/user.route');
+const actor = require('./routes/actor.route')
 
 ////////////////////////////////// ROUTEUR ///////////////////////////////////////
 
-app.use("/user", users);
+app.use("/user", user);
+app.use("/actor", actor)
 
 
 
 
 
-
+///////////////////////////////// LISTENER ///////////////////////////////////////
 app.listen(9090);
