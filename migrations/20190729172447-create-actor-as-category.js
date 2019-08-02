eb2453f327665d58f0121542cd244201f26b6011
PulcoOx - Home <oominimathoo@gmail.com>
2019-08-02 18:56:06 +0200

    changement AstonProjet:Bdd pour nouvelle tables

--- migrations/20190729172447-create-actor-as-category.js
+++ migrations/20190729172447-create-actor-as-category.js
@@ -1,38 +1,38 @@
 'use strict';
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('ActorAsCategories', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      actorId: {
-        type: Sequelize.INTEGER,
-        references: {
-          model: "Actors",
-          key: "id"
-        }
-      },
-      categoryId: {
-        type: Sequelize.INTEGER,
-        references: {
-          model: "Categories",
-          key: "id"
-        }
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('ActorAsCategories');
-  }
+    up: (queryInterface, Sequelize) => {
+        return queryInterface.createTable('ActorAsCategories', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            actorId: {
+                type: Sequelize.INTEGER,
+                references: {
+                    model: "Actors",
+                    key: "id"
+                }
+            },
+            categoryId: {
+                type: Sequelize.INTEGER,
+                references: {
+                    model: "Categories",
+                    key: "id"
+                }
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            }
+        });
+    },
+    down: (queryInterface, Sequelize) => {
+        return queryInterface.dropTable('ActorAsCategories');
+    }
 };
--- models/actor.js
+++ models/actor.js
@@ -1,35 +1,35 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const Actor = sequelize.define('Actor', {
-    name: DataTypes.STRING,
-    geo: DataTypes.JSON,
-    adress: DataTypes.STRING,
-    city: DataTypes.STRING,
-    cp: DataTypes.STRING,
-    country: DataTypes.STRING,
-    category: DataTypes.JSON,
-    website: DataTypes.STRING,
-    description: DataTypes.STRING,
-    telephonnumber: DataTypes.INTEGER,
-    openhours: DataTypes.STRING
-  }, {});
-  Actor.associate = function(models) {
-    // associations can be defined here
-    Actor.hasMany(models.Favori,{
-      foreignKey: "actorId"
-    });
-    Actor.hasMany(models.Comment,{
-      foreignKey: "actorId"
-    });
-    Actor.hasMany(models.Event,{
-      foreignKey: "actorId"
-    });
+    const Actor = sequelize.define('Actor', {
+        name: DataTypes.STRING,
+        geoLongitude: DataTypes.STRING,
+        geoLatitude: DataTypes.STRING,
+        adress: DataTypes.STRING,
+        city: DataTypes.STRING,
+        cp: DataTypes.STRING,
+        country: DataTypes.STRING,
+        website: DataTypes.STRING,
+        description: DataTypes.STRING,
+        telephonnumber: DataTypes.INTEGER,
+        openhours: DataTypes.STRING
+    }, {});
+    Actor.associate = function(models) {
+        // associations can be defined here
+        Actor.hasMany(models.Favori, {
+            foreignKey: "actorId"
+        });
+        Actor.hasMany(models.Comment, {
+            foreignKey: "actorId"
+        });
+        Actor.hasMany(models.Event, {
+            foreignKey: "actorId"
+        });
 
 
-    Actor.hasMany(models.Category,{
-      trough:'ActorAsCategory',
-      foreignKey: "actorId"
-    });
-  };
-  return Actor;
+        Actor.hasMany(models.Category, {
+            trough: 'ActorAsCategory',
+            foreignKey: "actorId"
+        });
+    };
+    return Actor;
 };
