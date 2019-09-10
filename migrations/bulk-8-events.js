4a54c3d43752750bb19425c741fd927a361f5e32
PulcoOx <oominimathoo@gmail.com>
2019-09-10 16:12:35 +0200

    fix text

--- migrations/bulk-8-events.js
+++ migrations/bulk-8-events.js
@@ -6,7 +6,7 @@ module.exports = {
             {
                 id : 1,
                 title: 'Braderie de Lille',
-                content: 'Il s\'agit de la plus grande braderie D\'Europe',
+                content: 'Il s\'agit de la plus grande braderie d\'Europe',
                 datebegin: '2019-08-31 12:00:00',
                 dateend: '2019-09-01 12:00:00',
                 location: 'Lille',
